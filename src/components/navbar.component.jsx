624cb4b1acd411711a977521bbeb84f9906f847f
projeksainsdata <projeksainsdata@gmail.com>
2024-05-31 15:43:33 +0700

    perbaikan 10

--- src/components/navbar.component.jsx
+++ src/components/navbar.component.jsx
@@ -85,21 +85,18 @@ const Navbar = () => {
 
                 
                 <div className={"absolute w-full left-0 top-full mt-0.5 border-b border-grey py-4 px-[5vw] md:border-0 md:block md:relative md:inset-0 md:p-0 md:w-auto md:show " + ( searchBoxVisibility ? "show" : "hide" )}>
-                    <input 
-                        type="text"
-                        placeholder="Cari..."
-                        className="w-full md:w-auto bg-grey p-4 pl-6 pr-[12%] md:pr-6 rounded-full placeholder:text-dark-grey md:pl-12"
-                        onKeyDown={handleSearch}
-                    />
-
-                    <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+                    <button className="bg-grey w-12 h-12 rounded-full flex items-center justify-center"
+                        onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}
+                        >
+                            <i className="fi fi-rr-search text-xl"></i>
+                    </button>
                 </div>
 
                 <div className="flex items-center gap-3 md:gap-4 ml-auto">
-                    <button className="md:hidden bg-grey w-12 h-12 rounded-full flex items-center justify-center"
-                    onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}
-                    >
-                        <i className="fi fi-rr-search text-xl"></i>
+                    <button className="bg-grey w-12 h-12 rounded-full flex items-center justify-center"
+                        onClick={() => setSearchBoxVisibility(currentVal => !currentVal)}
+                        >
+                            <i className="fi fi-rr-search text-xl"></i>
                     </button>
 
                     <Link to="/projek-kami" className="hidden md:flex gap-2 link">
@@ -112,27 +109,26 @@ const Navbar = () => {
                         <strong >Case Study</strong>
                     </Link>
 
+                    <Link to="/learn" className="hidden md:flex gap-2 link">
+                        <i className="fi fi-rr-books"></i>
+                        <strong >PSD-Learn</strong>
+                    </Link>
+
+                    <Link to="/dashboard/tanya-psd" className="hidden md:flex gap-2 link" onClick={handleNavLinkClick}>
+                        <i className="fi fi-rr-brain-circuit"></i>
+                        <strong >PSD-GPT</strong>
+                    </Link>
+
                     <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10" onClick={changeTheme}>
                         <i className={"fi fi-rr-" + ( theme == "light" ?  "bulb" : "brightness" ) + " text-2xl block mt-1 bg-dark" }></i>
                     </button>
 
                     <Link to="/editor" className="flex-none w-10">
                     <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
-                        <i className="fi fi-rr-brain-circuit text-2xl block mt-1 bg-dark"></i>
-                    </button>
-                    </Link>
-
-                    <Link to="/learn" className="flex-none w-10">
-                    <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
-                        <i className="fi fi-rr-books text-2xl block mt-1 bg-dark"></i>
-                    </button>
-                    </Link>
-
-                    <Link to="/dashboard/tanya-psd" className="flex-none w-10">
-                    <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
                         <i className="fi fi-rr-file-edit text-2xl block mt-1 bg-dark"></i>
                     </button>
                     </Link>
+                    
 
                     <Link to="/center" className="flex-none w-10">
                     <button className="w-12 h-12 rounded-full bg-grey relative hover:bg-black/10">
