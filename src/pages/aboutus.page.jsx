6504e534a09ed92e7ea879762c625f0eaf88b144
projeksainsdata <projeksainsdata@gmail.com>
2024-05-31 20:20:53 +0700

    about us

--- src/pages/aboutus.page.jsx
+++ src/pages/aboutus.page.jsx
@@ -14,12 +14,6 @@ const TeamMemberCard = ({ name, position }) => {
 const AboutUsPage = () => {
   const teamMembers = [
     { name: 'Ardika Satria', position: 'Founder' },
-    { name: 'Miftahul Huda', position: 'CEO' },
-    { name: 'Apri Arami', position: 'CFO' },
-    { name: 'Eggi Satria', position: 'CTO' },
-    { name: 'Virdio Samuel', position: 'Team CTO' },
-    { name: 'Happy Syahrul Ramadhan', position: 'Team CTO' },
-    { name: 'Wulan Sabina', position: 'CMO' },
   ];
 
   return (
@@ -29,10 +23,10 @@ const AboutUsPage = () => {
           <Col>
             <h2 className="text-2xl font-semibold mb-5">Tentang Projek Sains Data</h2>
             <p className="text-base mb-5">
-              Sains Data, telah menjadi salah satu bidang yang paling penting dan menarik dalam era teknologi informasi saat ini. Proyek sains data merupakan upaya komprehensif untuk mengumpulkan, mengorganisir, menganalisis, dan menginterpretasikan data untuk mendapatkan wawasan ataupun informasi. Tujuannya adalah mengidentifikasi pola, tren, dan informasi tersembunyi yang dapat digunakan untuk mengambil keputusan berdasarkan bukti, memahami perilaku pelanggan, meningkatkan efisiensi operasional, atau bahkan meramalkan tren masa depan. Dengan peluang yang tak terbatas dan potensi besar untuk memberikan solusi inovatif dalam berbagai domain, projek sains data lahir sebagai platform yang menyelesaikan permasalahan dengan pendekatan pembelajaran dan melalui riset inovatif dalam dunia yang semakin dipermeabel dengan data.
+              Projek Sains Data merupakan start up open platform atau platform terbuka untuk penulis yang memiliki peran dan antusias di bidang ilmu data mulai dari akademisi hingga profesional. Projek Sains Data atau PSD membuka ruang untuk belajar sains data yang lebih komprehensif, seperti sumber bacaan dari penulis blog komunitas, pembelajaran yang disusun berdasarkan domain sains data, buku original, tutorial penyelesaian studi kasus dengan data real, dan repositori proyek yang memuat latihan dari penggunaan konsep dan teori di sains data. Selain itu Projek Sains Data memiliki bisnis yang bergerak di bidang teknologi digital twin dengan kecerdasan buatan yang disebut Projek Sains Data Craft atau PSD-C. Fokus utama dari PSD-C adalah memberikan solusi untuk keputusan bisnis pada domain Energi, Lingkungan, Pertanian, Kesehatan, dan Manufaktur. PSD-C juga memberikan layanan konsultasi profesional untuk pengerjaan proyek sains data di industri maupun pendampingan riset. PSD memiliki komunitas diskusi yang disebut Diskusi Projek Sains Data atau DPSD. Komunitas ini memuat program yang menghubungkan para anggota untuk mendapatkan pelatihan, seminar, dan event tertentu, serta peluang koneksi kerjasama satu sama lain.
             </p>
             <p className="text-base">
-              Projek Sains Data lahir pada tanggal 03 Oktober 2022. Tempat dan kedudukan berada di Bandar Lampung, Lampung, Indonesia. Sifat dan status dari Projek Sains Data merupakan Start Up bidang data, sedangkan Diskusi Data merupakan Komunitas yang memuat komunitas belajar Sains Data.
+            PSD berkomitmen penuh untuk meningkatkan digital literasi di Indonesia dalam pengetahuan ilmu data agar adaptasi teknologi berbasis kecerdasan buatan yang berkembang menyesuaikan industri dunia saat ini menjadi bagian yang dapat diterima dengan baik khususnya oleh masyarakat Indonesia. Memulai dengan memberikan pemahaman yang terstruktur dan berdasarkan riset, PSD mendorong terciptanya dan terwujudnya inovasi berkelanjutan di bidang ilmu data. Sehingga solusi melalui pendekatan ilmu data tetap berkembang dan penerapannya tidak disalahgunakan. Memprediksi masa depan tentu menjadi bagian penting dalam mempersiapkan masa sekarang dan tidak mengulangi kesalahan di masa lalu, bergabunglah dengan berkontribusi pada blog dan pembelajaran sains data di Projek Sains Data yang terhubung dengan Diskusi Projek Sains Data, lalu ikut menciptakan solusi proyek sains data masa depan dengan PSD-C. Projek Sains Data lahir pada tanggal 03 Oktober 2022. Tempat dan kedudukan berada di Bandar Lampung, Lampung, Indonesia. Sifat dan status dari Projek Sains Data merupakan Start Up bidang data. 
             </p>
           </Col>
         </Row>
@@ -40,48 +34,23 @@ const AboutUsPage = () => {
           <Col md={6}>
             <div className="bg-white rounded-lg shadow p-4 mb-4">
               <h2 className="text-2xl font-semibold">Visi</h2>
-              <p className="text-base">Membangun Sains Data untuk Negeri dan Global melalui pembelajaran, inovasi, dan implementasi yang sustainable, dan responsible.</p>
+              <p className="text-base">Menjadi pusat pembelajaran sains data dan pusat pengembangan inovasi teknologi kembaran digital berbasis kecerdasan buatan yang diakui dunia</p>
             </div>
           </Col>
           <Col md={6}>
             <div className="bg-white rounded-lg shadow p-4 mb-4">
               <h2 className="text-2xl font-semibold">Misi</h2>
               <ul className="list-disc pl-5 text-base">
-                <li>Membagi konten belajar bagaimana menyelesaikan masalah di berbagai domain sains data dengan metode saintifik dan berdasarkan data baik teknis maupun konsep.</li>
-                <li>Meningkatkan produk sustainable development melalui penelitian sains data dan pengabdian masyarakat yang memiliki sifat responsible innovation.</li>
-                <li>Mewadahi forum diskusi projek sains data yang berkelanjutan untuk membahas dan mengkaji dasar dan penerapan teknis proyek sains data agar dapat menyumbangkan inovasi dan kreativitas dibidang ilmu data dan portofolio di bidang data.</li>
+                <li>Mengembangkan platform basis pengetahuan dan forum diskusi untuk mendukung kemajuan teknologi kecerdasan buatan dan pembelajaran ilmu sains data</li>
+                <li>Memberikan pelayanan profesional dengan solusi yang efektif dan efisien untuk masyarakat dan industri.</li>
+                <li>Mengembangkan produk teknologi kembaran digital yang mengedepankan inovasi berkelanjutan dengan mengadopsi teknologi kecerdasan buatan untuk memberikan wawasan dan prediksi yang akurat guna mendukung pengambilan keputusan bisnis, mengoptimalkan proses, dan meningkatkan efisiensi</li>
               </ul>
             </div>
           </Col>
         </Row>
         <Row className="mt-5">
           <Col>
-            <h3 className="text-xl font-semibold">Jadwal dan Program Projek Sains Data</h3>
-            <ul className="list-disc pl-5 text-base">
-              <li>Pembelajaran (Learning Path) merupakan program yang memuat jalur-jalur belajar sains data yang disusun dalam kurikulum projek sains data yaitu BARISTA (Basic - Analytics - Robotic - Intelligence - Softskill - Technology - Advanced Value), bentuk keluaran program ini adalah artikel ilmiah atau video.</li>
-              <li>Inovasi (Roadmap Projek Sains Data) program ini merupakan penelitian yang menekankan pada inovasi dalam ilmu data. Keluaran berupa Artikel Ilmiah, Essay, Dataset, Perangkat Pintar, Aplikasi Perangkat Lunak.</li>
-              <li>Sustainable (Aksi untuk SDGs) Sebagai generasi muda tentunya memiliki peran penting untuk menentukan nasib dari sebuah negara. Berlandasan SDGs, kami para generasi muda akan memberikan kontribusi nyata untuk mewujudkan SDGs demi kepentingan perdamaian dan kesejahteraan dunia. Kegiatan disajikan dalam Artikel, Podcast dan Webinar.</li>
-              <li>Responsible (Pengabdian Masyarakat dan Kegiatan Bersama Anggota) Program ini bertujuan untuk meningkatkan solidaritas dan membagi ilmu pengetahuan kepada para anggota serta melatih kemampuan para anggota untuk menciptakan portofolio di bidang ilmu data. Kemudian ikut serta dalam membantu dan memecahkan masalah yang ada di sekitar kita melalui ilmu data. Kegiatan dibuat dalam Workshop, Meeting, Podcast, Pengabdian, dan Proyek.</li>
-            </ul>
-          </Col>
-        </Row>
-        <Row className="mt-5">
-          <Col>
-          <ul className="list-disc pl-8 text-base"></ul>
-          <p>Jadwal Projek Sains Data:</p>
-            <li>Senin Belajar Data. </li>
-            <li>Senin Artikel Ilmiah. </li>
-            <li>Rabu Cari Data. </li>
-            <li>Rabu Solusi Statistik. </li>
-            <li>Kamis Infografis. </li>
-            <li>Kamis Codingan Manis. </li>
-            <li>Sabtu Codingan Pintar. </li>
-            <li>Sabtu SDGs. </li>
-          </Col>
-        </Row>
-        <Row className="mt-5">
-          <Col>
-            <h3 className="text-xl font-semibold">Tim PSD (Projek Sains Data)</h3>
+            <h3 className="text-xl font-semibold">Tim Projek Sains Data</h3>
             <p className="text-base mb-4">Meet our dedicated team of professionals who work tirelessly to achieve our goals.</p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
               {teamMembers.map((member, index) => (
