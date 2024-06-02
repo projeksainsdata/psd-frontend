8aa3c90957f9b4ee85c7c786c10956bb744dce1f
projeksainsdata <projeksainsdata@gmail.com>
2024-06-02 20:32:24 +0700

    404

--- src/pages/404.page.jsx
+++ src/pages/404.page.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import lightPageNotFoundImg from "../imgs/404-light.png";
-import darkPageNotFoundImg from "../imgs/404-dark.png";
+import lightPageNotFoundImg from "../imgs/404-psd.png";
+import darkPageNotFoundImg from "../imgs/404-psd.png";
 import lightFullLogo from "../imgs/psdDark.png";
 import darkFullLogo from "../imgs/PSDNormal.png";
 import { ThemeContext } from "../App";
@@ -13,7 +13,7 @@ const PageNotFound = () => {
     return (
         <section className="h-cover relative p-10 flex flex-col items-center gap-20 text-center">
 
-            <img src={ theme == "light" ? darkPageNotFoundImg : lightPageNotFoundImg } className="select-none border-2 border-grey w-72 aspect-square object-cover rounded" />
+            <img src={ theme == "light" ? darkPageNotFoundImg : lightPageNotFoundImg } className="select-none w-72 aspect-square object-cover rounded" />
 
             <h1 className="text-4xl font-gelasio leading-7">Page not found</h1>
             <p className="text-dark-grey text-xl leading-7 -mt-8">The page you are looking for does not exists. Head back to the <Link to="/" className="text-black underline">home page</Link></p>
