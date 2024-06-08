6dc20cd946e257aac19d67683c722bd4fddcd76e
projeksainsdata <projeksainsdata@gmail.com>
2024-06-08 18:41:56 +0700

    navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,18 @@ export default {
               gelasio: ["'Gelasio'", "serif"],
               monospace: ["Menlo, Monaco, Lucida Console, Liberation Mono, DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace"]
             },
+            colors: {
+                'purple-pink-gradient': 'linear-gradient(to right, #805AD5, #FA709A)',
+                'green-blue-gradient': 'linear-gradient(to right, #00C9A7, #00B4D8)',
+                'yellow-red-gradient': 'linear-gradient(to right, #FCE38A, #F38181)',
+                'teal-cyan-gradient': 'linear-gradient(to right, #17EAD9, #6078EA)',
+                'orange-pink-gradient': 'linear-gradient(to right, #FAD961, #F76B1C)',
+                'purple-indigo-gradient': 'linear-gradient(to right, #5D26C1, #A17FE0)',
+                'green-teal-gradient': 'linear-gradient(to right, #11998E, #38EF7D)',
+                'blue-purple-gradient': 'linear-gradient(to right, #0575E6, #021B79)',
+                'pink-red-gradient': 'linear-gradient(to right, #FF416C, #FF4B2B)',
+                'yellow-green-gradient': 'linear-gradient(to right, #F8FF00, #3AD59F)'
+            }
         },
 
     },
@@ -40,7 +52,8 @@ export default {
                 'dark-red': '#8B0000',
                 'pink':'#FF91D5',
                 'ungu':'#403178',
-                'discord': '#7289da'
+                'discord': '#7289da',
+                'apps' : '#2CBCAB'
             },
             dark: {
                 'white': '#2D3748',
@@ -56,7 +69,8 @@ export default {
                 'dark-red': '#8B0000',
                 'pink':'#FF91D5',
                 'ungu':'#403178',
-                'discord': '#7289da'
+                'discord': '#7289da',
+                'apps' : '#4476B9'
             } 
         })
     ],
