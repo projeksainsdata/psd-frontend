5ca6d69c1ca094abde12c6c527df2e0849d5e8c2
projeksainsdata <projeksainsdata@gmail.com>
2024-06-17 05:27:24 +0700

    trending

--- src/components/nobanner-blog-post.component.jsx
+++ src/components/nobanner-blog-post.component.jsx
@@ -6,18 +6,22 @@ const MinimalBlogPost = ({ blog, index }) => {
     
     let { title, blog_id: id, author: { personal_info: { fullname, username, profile_img } }, publishedAt } = blog;
 
+    // Function to capitalize each word in fullname
+    const capitalizeFullname = (name) => {
+        return name.replace(/\b\w/g, char => char.toUpperCase());
+    };
+
     return (
         <Link to={`/blog/${id}`} className="flex gap-5 mb-8">
             <h1 className="blog-index text-light-green">{ index < 10 ? "0" + (index + 1) : index === 7 ? "07" : index}</h1>
 
             <div>
-                <div className="flex gap-2 items-center mb-7">
+                <div className="flex gap-2 items-center mb-2">
                     <img src={profile_img} className="w-6 h-6 rounded-full" />
-                    <p className="line-clamp-1">{fullname} @{username}</p>
-                    <p className="min-w-fit">{ getDay(publishedAt) }</p>
+                    <p className="line-clamp-1">{capitalizeFullname(fullname)}</p>
                 </div>
 
-                <h1 className="blog-title font-bold">{title}</h1>
+                <h1 className="blog-trending font-bold">{title}</h1>
             </div>
         </Link>
     )
